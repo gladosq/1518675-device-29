26fbfc93cae604bde85894cb4238ad6799d90488
gladosq <gladosq@gmail.com>
2020-10-22 00:24:06 +0400

    Декоративные элементы на внутренней странице
    Декоративные элементы для внутренней страницы.

---
+++ js/feedback.js
@@ -0,0 +1,33 @@
+const feedbackButton = document.querySelector('.button-feedback');
+const feedbackPopup = document.querySelector('.modal-feedback');
+const feedbackClose = document.querySelector('.modal-close-button');
+const feedbackForm = document.querySelector('.feedback-form');
+const feedName = document.querySelector('.feed-name');
+const feedEmail = document.querySelector('.feed-email');
+const feedText = document.querySelector('.feed-text');
+
+feedbackButton.addEventListener('click', function (evt) {
+	evt.preventDefault();
+	feedbackPopup.classList.add('modal-show');
+});
+
+feedbackClose.addEventListener('click', function (evt) {
+	evt.preventDefault();
+	feedbackPopup.classList.remove('modal-show');
+});
+
+feedbackForm.addEventListener('submit', function (evt) {
+	if (!feedName.value || !feedEmail.value || !feedText.value) {
+		evt.preventDefault();
+		console.log('ASFASFASF FORAMA');
+	} 
+});
+
+window.addEventListener('keydown', function (evt){
+	if (evt.keyCode === 27) {
+		if (feedbackPopup.classList.contains('modal-show')) {
+			evt.preventDefault();
+			feedbackPopup.classList.remove('modal-show');
+		}
+	}
+});
